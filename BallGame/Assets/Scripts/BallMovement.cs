43de3c3a2f6479f733b4fa46159497cc28bdff62
d15125895 <33666054+d15125895@users.noreply.github.com>
2018-04-19 14:38:55 +0100

    ball player movement

--- BallGame/Assets/Scripts/BallMovement.cs
+++ BallGame/Assets/Scripts/BallMovement.cs
@@ -7,11 +7,26 @@ public class BallMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sideWaysForce = 500f;
+
     // marked this with fixedUpdate becaus; iam using it to mess with the physics.
     void FixedUpdate()
     {
         // add force 2000 to the Z axis
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
+        if (Input.GetKey("l"))
+        {
+
+            rb.AddForce(sideWaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+
+        {
+            rb.AddForce(-sideWaysForce * Time.deltaTime, 0, 0);
+        }
+        
     }
 }
